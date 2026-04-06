8274f863f9c684b9c9cd952627f65e4efd86d6c9
SOTHEA Reach <sotheareach@SOTHEAs-MacBook-Pro.local>
2026-04-06 22:16:55 +0700

    updated

--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -20,7 +20,7 @@ export class AssetsService {
         uploadedById?: string,
     ) {
         const { key, url } = await this.s3.upload(file, folder);
-        console.log('Uploaded file to S3:', { key, url });
+        // console.log('Uploaded file to S3:', { key, url });
         return this.prisma.asset.create({
             data: {
                 fileName: file.originalname,
